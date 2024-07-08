69c2e807f1e47572d012727f5909a2505d48cefe
SergioLM <sergiolillom@gmail.com>
2024-07-08 23:43:22 +0200

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Main from './components/Main'
 
 function App() {
-
-  const [count, setCount] = useState(0)
 
   return (
     <>
